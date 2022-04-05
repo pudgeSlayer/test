927edebc0803b815d5e63a11a87ec359030884e5
pudgeSlayer <ddaniilych@gmail.com>
2022-04-05 21:52:40 +0300

    ...

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-var mass = [2, 4, -5, 0, -7, 1];
-mass.forEach((i) => {
-  var b = "";
-  if (i > 0) {
-    for (let f = 0; f < i; f++) {
-      b = b + "a";
-    }
-  } else if (i < 0) {
-    for (let f = 0; f < -i; f++) {
-      b = b + "b";
-    }
-  }
-  if (i != 0) {
-    console.log(b);
-  }
-});
+var mass = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11];
+for (let i = 0; i < mass.length; i++) {
+  mass[i] = mass[mass.length - i - 1];
+}
+console.log(mass);
