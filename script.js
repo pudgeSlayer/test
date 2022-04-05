5917d3838f334c9639bbf2fc32a3477a8842742e
pudgeSlayer <ddaniilych@gmail.com>
2022-04-05 21:42:10 +0300

    ...

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-var a = 3;
-var c = 4;
-
-function summ(a, c) {
-  console.log(a + c);
-}
-summ(4, 6)
+var mass = [2, 4, -5, 0, -7, 1];
+mass.forEach((i) => {
+  var b = "";
+  if (i > 0) {
+    for (let f = 0; f < i; f++) {
+      b = b + "a";
+    }
+  } else if (i < 0) {
+    for (let f = 0; f < -i; f++) {
+      b = b + "b";
+    }
+  }
+  if (i != 0) {
+    console.log(b);
+  }
+});
